a861c388977f3fa8069f04df752e34051c262b38
nthnn <nathanneisip@gmail.com>
2023-05-29 18:58:09 +0800

    Improved web wrapper, automation, and more.

---
+++ build/asset-mover.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+robocopy "../src/app/Talk2JogRat-Sketch/dist" "../src/app/Talk2JogRat/app/src/main/assets" /E
+pause
--- src/app/Talk2JogRat/app/src/main/java/xyz/nathannestein/talk2jograt/MainActivity.kt
+++ src/app/Talk2JogRat/app/src/main/java/xyz/nathannestein/talk2jograt/MainActivity.kt
@@ -2,14 +2,34 @@ package xyz.nathannestein.talk2jograt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView;
+import android.os.Handler
+import android.webkit.WebView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var shouldExit: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView: WebView = findViewById(R.id.main_web_view);
+        val webView: WebView = findViewById(R.id.main_web_view)
         webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file://android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        when {
+            this.shouldExit -> this.finish()
+            else -> {
+                Toast.makeText(this, "Press back again to exit.", Toast.LENGTH_SHORT).show()
+                shouldExit = true
+
+                Handler().postDelayed({
+                    shouldExit = false
+                }, 3000)
+            }
+        }
+        this.finish()
     }
 }
