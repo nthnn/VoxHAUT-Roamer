01142ef36e12557e51117205fd2e12baa98c6b4f
nthnn <nathanneisip@gmail.com>
2023-05-30 19:18:37 +0800

    Smooth WebView wrapper.

--- build/clean-up.bat
+++ build/clean-up.bat
@@ -4,4 +4,6 @@ cd ../src
 rmdir /s "./app/Talk2JogRat-Sketch/.cache"
 rmdir /s "./app/Talk2JogRat-Sketch/dist"
 rmdir /s "./app/Talk2JogRat/app/build"
+rmdir /s "./app/Talk2JogRat/app/src/main/assets"
+mkdir "./app/Talk2JogRat/app/src/main/assets"
 pause
--- src/app/Talk2JogRat/app/src/main/java/xyz/nathannestein/talk2jograt/MainActivity.kt
+++ src/app/Talk2JogRat/app/src/main/java/xyz/nathannestein/talk2jograt/MainActivity.kt
@@ -3,19 +3,34 @@ package xyz.nathannestein.talk2jograt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.webkit.WebView
+import android.view.Window
+import android.webkit.*
 import android.widget.Toast
+
+import androidx.webkit.WebViewAssetLoader;
 
 class MainActivity : AppCompatActivity() {
     private var shouldExit: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.setContentView(R.layout.activity_main)
 
         val webView: WebView = findViewById(R.id.main_web_view)
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build()
+
+        webView.webViewClient = LocalWebViewClient(assetLoader)
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.settings.allowFileAccessFromFileURLs = true
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("file://android_asset/index.html")
+        webView.webViewClient = LocalWebViewClient(assetLoader)
+
+        webView.loadUrl("file:///android_asset/index.html")
     }
 
     override fun onBackPressed() {
@@ -30,6 +45,5 @@ class MainActivity : AppCompatActivity() {
                 }, 3000)
             }
         }
-        this.finish()
     }
 }
