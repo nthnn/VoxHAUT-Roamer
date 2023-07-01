da0a846cee63d8ca408f2b2729e057cf8ff15cb6
nthnn <nathanneisip@gmail.com>
2023-07-02 01:14:26 +0800

    Animation and element view for message options dialog.

--- src/app/Talk2VoxHAUT-Sketch/src/app/FloatingCircleButton.tsx
+++ src/app/Talk2VoxHAUT-Sketch/src/app/FloatingCircleButton.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+
+import MessageOptionsDialog from './MessageOptionsDialog';
 
 export default class FloatingCircleButton extends React.Component {
+    showOptions(): void {
+        let root = ReactDOMClient.createRoot(document.getElementById('dialog-options'));
+        $("#dialog-options")
+            .removeClass("d-none")
+            .removeClass("animate__slideOutUp");
+
+        root.render(<MessageOptionsDialog />);
+        $("#dialog-options").addClass("animate__slideInDown");
+    }
+
     render() {
         return (
-            <div className="floating-btn animate__animated animate__slow animate__fadeIn" align="center">
+            <div className="floating-btn animate__animated animate__slow animate__fadeIn"
+                align="center"
+                onClick={this.showOptions}>
+
                 <img src="images/ic-add-btn.png" width="28" />
             </div>
         );
