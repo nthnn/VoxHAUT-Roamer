70cb3f5da3d29afe51f2c8d9b1ea28619898bc84
nthnn <nathanneisip@gmail.com>
2023-07-06 22:40:12 +0800

    Removed unused variable.

--- src/firmware/VoxHAUT-Sys/src/voxhaut_ultrasonic_sensor.cpp
+++ src/firmware/VoxHAUT-Sys/src/voxhaut_ultrasonic_sensor.cpp
@@ -1,8 +1,6 @@
 #include "voxhaut_ultrasonic_sensor.h"
 
 int VoxHAUTUltrasonicSensor::get_distance() {
-    int duration = 0;
-
     digitalWrite(this->_trig_pin, LOW);
     delayMicroseconds(2);
 
